5cc3f8af63cf4f1a50429f391621a9ec8ad6c159
FF <supakhon2549@gmail.com>
2026-04-15 20:56:10 +0700

    Get to be Node.js not .py

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config(); // for environment || .env
+
 const express = require('express');
 const app = express();
 
-const db = require("./db");
+const db = require("./db"); // for database
+
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -14,6 +17,41 @@ app.post('/input', (req, res) => {
     });
 });
 
+// app.post("/message", async (req, res) => {
+//     try {
+//         const { username, msg_type, data } = req.body;
+
+//         const result = await db.query(
+//             "INSERT INTO messages (username ,msg_type, data) VALUES ($1, $2, $3) RETURNING *",
+//             [username || "anon", msg_type, data]
+//         );
+
+//         res.json(result.rows[0]);
+
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json({ error: "DB error" });
+//     }
+// });
+
+//
+
+
+// app.get("/messages", async (req, res) => {
+//     try {
+//         const result = await db.query(
+//             "SELECT * FROM messages ORDER BY created_at DESC LIMIT 50"
+//         );
+//         res.json(result.rows);
+//     } catch (err) {
+//         res.status(500).json({ error: "Cannot fetch messages" });
+//     }
+// });
+
+
+// (async () => {
+
+// })();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
