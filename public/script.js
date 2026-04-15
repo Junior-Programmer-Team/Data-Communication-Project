431a236a1000c925a010c6c295725cf6086e4cf8
FF <supakhon2549@gmail.com>
2026-04-15 18:24:08 +0700

    just want to try db

---
+++ public/script.js
@@ -0,0 +1,15 @@
+async function sendtoDB() {
+    const input_text = document.getElementById("inputTest").value;
+
+    const respone = await fetch('/input', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ input_text })
+    });
+
+    const data = await response.json();
+
+    console.log(data)
+
+    input_text.innerText = "";
+}
